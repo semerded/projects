03a3fd288dc803ed2a9e3878b64902ddbffc1094
Sem Van Broekhoven <sem.van.broekhoven@gmail.com>
2023-12-06 21:54:47 +0100

    update to pygameaddons

--- pyaddons/pygameAddons/pygameaddons.py
+++ pyaddons/pygameAddons/pygameaddons.py
@@ -2,8 +2,10 @@ try:
     import pygame
 except ImportError:
     raise ImportError("import pygame")
-import random, sys
+import random
+import sys
 from typing_extensions import TypeAlias, Literal
+from enum import Enum
 
 pygame.init()
 pygame.font.init()
@@ -13,44 +15,69 @@ modifiableFunctions: TypeAlias = Literal[
     "quit"
 ]
 
-colorValue = tuple[int,int,int]
+RGBvalue: TypeAlias = tuple[int, int, int]
+font : TypeAlias = pygame.font
+
+
+class mouseButton(Enum):
+    leftMouseButton = 1
+    rightMouseButton = 2
+    middleMouseButton = 3
+    unknownbutton1 = 4
+    unknownbutton2 = 5
+    mouseButton4 = 6
+    mouseButton5 = 7
+    
+class interactionType(Enum):
+    mouseOver = 1
+    mouseClick = 2
+    mouseRelease = 3
+    mouseHold = 4
+
+
 screenInfo = pygame.display.Info()
 userScreenWidth = screenInfo.current_w
 userScreenHeight = screenInfo.current_h
+mainDisplay = None
+
+mouseButtonsStatus = [False, False, False, False, False]
+previousMouseButtonStatus = [False, False, False, False]
+
 
 class Color:
-    #TODO add argb
-    RED = (255,0,0)
-    LAVARED = (255,40,0)
-    ORANGE = (255,80,0)
-    YELLOW = (255,255,0)
-    GREEN = (0,255,0)
-    OLIVEGREEN = (0,150,0)
-    DARKGREEN = (0,100,0)
-    TURQUISE = (0,255,255)
-    PINK = (255,0,100)
-    BLUE = (0,0,255)
-    LIGHTBLUE = (0,120,255)
-    PURPLE = (255,0,255)
-    WHITE = (255,255,255)
-    LESSWHITE = (200,200,200)
-    LESSRED = (200,0,0)
-    LESSYELLOW = (200,200,0)
-    LESSGREEN = (0,200,0)
-    LESSTURQUISE = (0,200,200)
-    LESSBLUE = (0,0,200)
-    LIGHTGRAY = LIGHTGREY = (150,150,150)
-    GRAY = GREY = (100,100,100)
-    DARKGRAY = DARKGREY = (50,50,50)
-    DARKMODEGRAY = DARKMODEGREY = (30,30,30)
-    BLACK = (0,0,0)
-    
+    # TODO add argb
+    RED = (255, 0, 0)
+    LAVARED = (255, 40, 0)
+    ORANGE = (255, 80, 0)
+    YELLOW = (255, 255, 0)
+    GREEN = (0, 255, 0)
+    OLIVEGREEN = (0, 150, 0)
+    DARKGREEN = (0, 100, 0)
+    TURQUISE = (0, 255, 255)
+    PINK = (255, 0, 100)
+    BLUE = (0, 0, 255)
+    LIGHTBLUE = (0, 120, 255)
+    PURPLE = (255, 0, 255)
+    WHITE = (255, 255, 255)
+    LESSWHITE = (200, 200, 200)
+    LESSRED = (200, 0, 0)
+    LESSYELLOW = (200, 200, 0)
+    LESSGREEN = (0, 200, 0)
+    LESSTURQUISE = (0, 200, 200)
+    LESSBLUE = (0, 0, 200)
+    LIGHTGRAY = LIGHTGREY = (150, 150, 150)
+    GRAY = GREY = (100, 100, 100)
+    DARKGRAY = DARKGREY = (50, 50, 50)
+    DARKMODEGRAY = DARKMODEGREY = (30, 30, 30)
+    BLACK = (0, 0, 0)
+
     def random():
         tuple = []
         for i in range(3):
             tuple.append(random.randint(0, 255))
         return (tuple[0], tuple[1], tuple[2])
-    
+
+
 class Font:
     H1 = pygame.font.SysFont(pygame.font.get_default_font(), 34)
     H2 = pygame.font.SysFont(pygame.font.get_default_font(), 30)
@@ -69,28 +96,30 @@ class Font:
     FONT50 = pygame.font.SysFont(pygame.font.get_default_font(), 50)
     FONT100 = pygame.font.SysFont(pygame.font.get_default_font(), 100)
     FONT150 = pygame.font.SysFont(pygame.font.get_default_font(), 150)
-    
-    def customFont(size, font = None):
+
+    def customFont(size, font=None):
         if font == None:
             return pygame.font.SysFont(pygame.font.get_default_font(), size)
         else:
             return pygame.font.SysFont(font, size)
-        
+
 
 class AppConstructor():
     def __init__(self, screenWidth, screenHeight, *flags) -> None:
+        global mainDisplay
         self.screenWidth = screenInfo.current_w
         self.screenHeight = screenInfo.current_h
         self.APPdisplayFlags = flags
-        
+
         self.minimumScreenWidth = None
         self.minimumScreenHeight = None
         self.modifiedFunctions = {"quit": None}
-        
-        self.APPdisplay = pygame.display.set_mode((screenWidth, screenHeight) ,*flags)
-        
-        
-    def eventHandler(self, appEvents:pygame.event):
+
+        self.APPdisplay = pygame.display.set_mode(
+            (screenWidth, screenHeight), *flags)
+        mainDisplay = self.APPdisplay
+
+    def eventHandler(self, appEvents: pygame.event):
         self.appEvents = appEvents
         for event in self.appEvents:
             if event.type == pygame.QUIT:
@@ -99,20 +128,26 @@ class AppConstructor():
                     sys.exit()
                 else:
                     self.modifiedFunctions["quit"]()
-                    
+
             if event.type == pygame.WINDOWRESIZED:
                 self._checkForMinimumScreenSizeBreaches()
-    
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouseButtonsStatus[event.button] = True
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouseButtonsStatus[event.button] = False
+
     def resizeAppscreen(self, screenWidth, screenHeight, *flags):
         self.APPdisplayFlags = flags
-        self.APPdisplay = pygame.display.set_mode((screenWidth, screenHeight), *flags)    
-    
-    
-    def setModifiableFunctions(self, **kwargs:modifiableFunctions): # TODO
+        self.APPdisplay = pygame.display.set_mode(
+            (screenWidth, screenHeight), *flags)
+
+    def setModifiableFunctions(self, **kwargs: modifiableFunctions):  # TODO
         for modifiedFunction, newFunction in kwargs.items():
             self.modifiedFunctions[modifiedFunction] = newFunction
-            
-    def setMinimumScreenDimensions(self, minimumScreenWidth:screenUnit = None, minimumScreenHeight:screenUnit = None):
+
+    def setMinimumScreenDimensions(self, minimumScreenWidth: screenUnit = None, minimumScreenHeight: screenUnit = None):
         if minimumScreenWidth != None:
             self.minimumScreenWidth = minimumScreenWidth
         else:
@@ -121,7 +156,7 @@ class AppConstructor():
             self.minimumScreenHeight = minimumScreenHeight
         else:
             self.minimumScreenHeight = None
-    
+
     def _checkForMinimumScreenSizeBreaches(self):
         w, h = self.getAppScreenDimensions
         updateScreen = False
@@ -133,20 +168,18 @@ class AppConstructor():
             updateScreen = True
         if updateScreen:
             self.resizeAppscreen(w, h, *self.APPdisplayFlags)
-        
-        
-        
-     
+
     @property
     def setRelativeFullscreen(self):
-        self.APPdisplay = pygame.display.set_mode((self.screenWidth, self.screenHeight), *self.APPdisplayFlags)
+        self.APPdisplay = pygame.display.set_mode(
+            (self.screenWidth, self.screenHeight), *self.APPdisplayFlags)
         self.setModifiableFunctions()
-        
+
     @property
     def getAppScreenDimensions(self):
         displayInfo = pygame.display.Info()
         return displayInfo.current_w, displayInfo.current_h
-    
+
     @property
     def getdisplayDimensions(self):
         return (self.screenWidth, self.screenHeight)
@@ -154,37 +187,17 @@ class AppConstructor():
     @property
     def maindisplay(self) -> pygame.surface:
         """pygame main surface"""
-        return self.APPdisplay            
-    
-    
-  
-class Image:
-    def resizeImage(image, size: tuple[int,int]):
-        aspectRatio = Image.getAspectRatio(image)
-        imageWidth = size[0]
-        imageHeight = size[1]
-        if aspectRatio < 1:
-            imageWidth = int(imageWidth * aspectRatio)
-        else:
-            imageHeight = int(imageHeight/ aspectRatio)
-            
-        
+        return self.APPdisplay
 
-    def getAspectRatio(image):
-        #TODO get size
-        imageWidth, imageHeight = image # getsize
-        aspectRatio = imageWidth / imageHeight
-        
-              
-  
+
 class Scroll():
     def __init__(self) -> None:
         pass
-    
+
     @property
     def set_scroll(self):
         self.__scrollActivated = True
-        
+
     @property
     def get_scroll(self):
         return self.__scrollActivated
@@ -193,93 +206,215 @@ class Scroll():
 class ScreenUnit:
     def convert():
         ...
-        
+
     def precent(parentSize, percent):
         return parentSize / 100 * percent
-    
-    def vw(screenUnit:float) -> float:
-        return userScreenWidth / 100 * screenUnit
-    
-    def vh(screenUint:float) -> float:
-        return userScreenHeight / 100 * screenUnit
-        
 
-        
-        
-        
-    
+    def vw(screenUnit: float) -> float:
+        return userScreenWidth / 100 * screenUnit
+
+    def vh(screenUint: float) -> float:
+        return userScreenHeight / 100 * screenUnit
+
+
+class Image:
+    def __init__(self, fileName: str, nameHint: str = "") -> None:
+        self.image = pygame.image.load(fileName, nameHint)
+        self.imageSize = self.getSize
+        self.imagePosition = (0, 0)
+
+    def resizeAndKeepProportions(self, width: float, height: float):
+        aspectRatio = self.getAspectRatio
+        imageWidth, imageHeight = self.getSize
+        if aspectRatio < 1:
+            width = int(imageWidth * aspectRatio)
+        else:
+            height = int(imageHeight / aspectRatio)
+        self.scale(width, height)
+
+    def scale(self, width, height):
+        self.image = pygame.transform.scale(self.image, (width, height))
+        self.imageSize = self.getSize
+
+    def isMouseOver(self):
+        return Interactions.isMouseOver(self.getImageRect)
+
+    def isClicked(self, mouseButton: mouseButton):
+        return Interactions.isClicked(self.getImageRect, mouseButton)
+
+    def isReleased(self, mouseButton: mouseButton):
+        return Interactions.isReleased(self.getImageRect, mouseButton)
+
+    def isHolding(self, mouseButton: mouseButton):
+        return Interactions.isHolding(self.getImageRect, mouseButton)
+
+    def place(self, xPosition: float, yPosition: float):
+        self.imagePosition = (xPosition, yPosition)
+        self.image.blit(mainDisplay, self.imagePosition)
+
+    @property
+    def getAspectRatio(self):
+        imageWidth, imageHeight = self.getSize
+        return imageWidth / imageHeight
+
+    @property
+    def getSize(self):
+        return self.image.get_size()
+
+    @property
+    def getPosition(self):
+        return self.imagePosition
+
+    @property
+    def getImageRect(self):
+        return pygame.Rect(*self.getPosition, *self.getSize)
+
+    @property
+    def getImage(self):
+        return self.image
+
+
+class Interactions:
+    def __init__(self) -> None:
+        pass
+
+    def isMouseButtonPressed(mouseButton: mouseButton):
+        if mouseButtonsStatus[mouseButton.value]:
+            return True
+        return False
+
+    def mouseButtonPositiveFlank(mouseButton: mouseButton):
+        if mouseButtonsStatus[mouseButton] and not previousMouseButtonStatus[mouseButton]:
+            previousMouseButtonStatus[mouseButton] = True
+            return True
+        return False
+
+    def mouseButtonNegativeFlank(mouseButton: mouseButton):
+        if not mouseButtonsStatus[mouseButton] and previousMouseButtonStatus[mouseButton]:
+            previousMouseButtonStatus[mouseButton] = False
+            return True
+        return False
+
+    def isMouseOver(rect: pygame.Rect):
+        mousePos = pygame.mouse.get_pos()
+        if rect.collidepoint(mousePos):
+            return True
+        return False
+
+    def isClicked(rect: pygame.Rect, mouseButton: mouseButton):
+        if Interactions.isMouseOver(rect) and Interactions.mouseButtonPositiveFlank(mouseButton.value):
+            return True
+        return False
+
+    def isReleased(rect: pygame.Rect, mouseButton: mouseButton):
+        if Interactions.isMouseOver(rect) and Interactions.mouseButtonNegativeFlank(mouseButton.value):
+            return True
+        return False
+
+    def isHolding(rect: pygame.Rect, mouseButton: mouseButton):
+        if Interactions.isMouseOver(rect) and Interactions.isMouseButtonPressed(mouseButton.value):
+            return True
+        return False
+            
+
 
 class Text:
 
+    pass
+
+    def place(self):
         pass
-        
-        def place(self):
-            pass
 
 
-    
 class Button:
     def __init__(self,
-                 size: tuple[screenUnit,screenUnit],
-                 textColor: tuple[int,int,int] = Color.BLACK,
-                 bgColor: colorValue = Color.LESSWHITE,
+                 size: tuple[screenUnit, screenUnit],
+                 textColor: RGBvalue = Color.BLACK,
+                 bgColor: RGBvalue = Color.LESSWHITE,
                  borderTickness: int = 2,
-                 borderColor: colorValue = Color.BLACK,
+                 borderColor: RGBvalue = Color.BLACK,
                  borderRadius: int = 0,
-                 onClick = ...,
-                 onHold = ...,
-                 onHover = ...,
-                 colorOnHover: colorValue = ...,
-                 
-                 
+                 colorOnHover: RGBvalue = ...
                  ) -> None:
-        print(onClick)
+        pass
+
+    def simpleButton(size,
+                     position, 
+                     backgroundColor: RGBvalue = Color.LIGHTGRAY, 
+                     text: str = "", 
+                     font: font = Font.H3,
+                     textColor: RGBvalue = Color.BLACK, 
+                     cornerRadius: int = -1, 
+                     borderWidth: int = 0,
+                     borderColor: RGBvalue = Color.BLACK,
+                     ) -> bool:
+        
+        buttonRect = Drawing.rectangle(position[0], position[1], size[0], size[1], backgroundColor, cornerRadius)
+        if borderWidth > 0:
+            buttonRect = Drawing.border(borderWidth, buttonRect, borderColor, cornerRadius)
+        if text != "":
+            buttonText = font.render(text, True, textColor)
+            mainDisplay.blit(buttonText, (position[0] + size[0] / 2 - buttonText.get_width() / 2, position[1] + size[1] / 2 - buttonText.get_height() / 2))
+        return Interactions.isClicked(buttonRect, mouseButton.leftMouseButton)
+        
+        
 
     def __text(self):
         ...
-        
+
     def __border(self):
         ...
-    
+
     def __radius(self):
         ...
-        
+
     def __resize__(self):
         ...
-    
+
     def __recolor__(self):
         ...
-        
+
     def onMouseOver(self):
         ...
-        
+
     def onMouseClick(self):
         ...
-        
+
     def onMouseHold(self):
         ...
-    
+
     def changeColorOnHover(self):
         ...
-        
+
     def place(self):
         ...
-        
-    
-        
-    
+
+
 class text:
     def __init__(self) -> None:
         pass
-    
+
     def centerd(self):
         ...
-        
-    
 
+class Drawing:
+    def rectangle(xPosition: float, yPosition: float, width: float, height: float, color: RGBvalue = Color.LIGHTGRAY, cornerRadius: int = -1):
+        return pygame.draw.rect(mainDisplay, color, pygame.Rect(xPosition, yPosition, width, height), border_radius=cornerRadius)
+    
+    def border(borderWidth: int, rectValue: pygame.Rect | tuple[float, float, float, float], color: RGBvalue = Color.BLACK, cornerRadius: int = -1) -> pygame.Rect:
+        if isinstance(rectValue, pygame.Rect):
+            xPosition = rectValue.x - borderWidth / 2
+            yPosition = rectValue.y - borderWidth / 2
+            width = rectValue.width + borderWidth
+            height = rectValue.height + borderWidth
+        else:
+            xPosition = rectValue[0] - borderWidth / 2
+            yPosition = rectValue[1] - borderWidth / 2
+            width = rectValue[2] - borderWidth
+            height = rectValue[3] - borderWidth
+        return pygame.draw.rect(mainDisplay, color, pygame.Rect(xPosition, yPosition, width, height), width=borderWidth, border_radius=cornerRadius)
+ 
+            
 class textbox:
     def __init__(self) -> None:
         pass
-    
- 
-    
